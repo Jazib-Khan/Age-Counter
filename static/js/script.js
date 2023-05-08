ea2c71ad80f999814b1e3b99a6391edaec1cbe2b
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-05-08 18:35:03 +0100

    age in days function implemented

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+
+function ageInDays(){
+    var birthYear = prompt("What year were you born?");
+    var ageInDayss = (2023 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+
